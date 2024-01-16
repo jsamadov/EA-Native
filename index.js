6f81151a4c949992cf40b49be929f36184f75c48
jsamadov <samedov838@gmail.com>
2024-01-17 03:31:27 +0400

    second add

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var fullname = "Hello World";
-var surname = "World";
-var age = 28;
+const personalProfile = {
+  firstName: "Paribus",
+  lastName: "Le Files",
+  age: 28,
+  favoriteLanguage: "kotlin",
+  contactInfo: {
+    email: "qwerty@zxc",
+    phone: "0123456",
+  },
+};
 
-fullname = "John";
-surname = "Italy"
-age="21"
+personalProfile["occupation"] = "esport";
+personalProfile["contactInfo"]["address"] = "Balakhani";
 
-console.log(fullname);
-console.log(surname)
+console.log(personalProfile);
+console.log(personalProfile.lastName);
+console.log(personalProfile.contactInfo.address);
+console.log(personalProfile.contactInfo["phone"]);
+console.log(personalProfile["occupation"]);
