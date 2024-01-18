c5b81f98a1fd0603e13e5cc5aad0361a14761e9a
jsamadov <samedov838@gmail.com>
2024-01-18 04:03:42 +0400

    update code
    add new exam js file

---
+++ exam.js
@@ -0,0 +1,32 @@
+// // task => exam level 1: Simple
+//   // part 1
+// const studentProfile = {
+//     name: "John",
+//     age: "15",
+//     course: "life hack",
+//     hobbies: ["origami", "gaming"],
+//   };
+
+//   delete studentProfile.course;
+//   console.log(studentProfile);
+
+//   // part 2
+//   const { name: studentName, age, ...rest } = studentProfile;
+
+//   console.log(studentName, age, rest);
+
+//   // part 3
+//   const grades = [85, 95, 78, 95];
+//   console.log(grades[0], grades[1]);
+
+//   // part 4
+//   const additionalInfo = {
+//     address: "balakhani",
+//     phoneNumber: "012345",
+//   };
+
+//   const newObj = {
+//     ...studentProfile,
+//     ...additionalInfo,
+//   };
+//   console.log(newObj);
--- index.js
+++ index.js
@@ -1,19 +1,63 @@
-const personalProfile = {
-  firstName: "Paribus",
-  lastName: "Le Files",
-  age: 28,
-  favoriteLanguage: "kotlin",
-  contactInfo: {
-    email: "qwerty@zxc",
-    phone: "0123456",
+// Lesson-4 Joys of JS
+
+// task-exam => 15/01/2024
+
+// const personalProfile = {
+//   firstName: "Paribus",
+//   lastName: "Le Files",
+//   age: 28,
+//   favoriteLanguage: "kotlin",
+//   contactInfo: {
+//     email: "qwerty@zxc",
+//     phone: "0123456",
+//   },
+// };
+
+// personalProfile["occupation"] = "esport";
+// personalProfile["contactInfo"]["address"] = "Balakhani";
+
+// console.log(personalProfile);
+// console.log(personalProfile.lastName);
+// console.log(personalProfile.contactInfo.address);
+// console.log(personalProfile.contactInfo["phone"]);
+// console.log(personalProfile["occupation"]);
+
+//Lesson-5 Jumping for JS
+
+// const user = {
+//   name: "asd",
+//   surname: "dsa",
+//   tax: {
+//     numberTax: "123",
+//   },
+// };
+
+// dest
+// const { name, surname } = user;
+// const { numberTax } = user.tax;
+
+// (... obyektin daxilinde olan prop'lari copy edir)
+// const { name, ...rest } = user;
+
+// console.log(rest);
+
+//task level 2 : Advance
+//part 1
+const employeeDetails = {
+  fullName: "John Wick",
+  position: "Actor",
+  yearsOfExperience: "4",
+  skills: ["pen", "gun", "knife"],
+  contact: {
+    email: "johnwick@gun.az",
+    phone: "012345",
   },
+  department: "Constantine",
 };
 
-personalProfile["occupation"] = "esport";
-personalProfile["contactInfo"]["address"] = "Balakhani";
+delete employeeDetails.contact.email;
+console.log(employeeDetails);
 
-console.log(personalProfile);
-console.log(personalProfile.lastName);
-console.log(personalProfile.contactInfo.address);
-console.log(personalProfile.contactInfo["phone"]);
-console.log(personalProfile["occupation"]);
+// Part 2
+const { fullName: employeeName, position, contact, ...rest } = employeeDetails;
+console.log(employeeName, position, contact, rest);
