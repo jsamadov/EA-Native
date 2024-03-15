86093acace02b164dcc6f45955bddfe4dc6bfe96
jsamadov <samedov838@gmail.com>
2024-03-15 17:15:40 +0400

    request method
    API

--- task/request.js
+++ task/request.js
@@ -1,49 +1,90 @@
-// https://api.chucknorris.io/jokes/random
-// https://simple-grocery-store-api.glitch.me
-// https://www.boredapi.com/api/
-// https://api.thecatapi.com/
-// http://www.omdbapi.com/?apikey=[yourkey]&
-// https://restcountries.com/v3.1/all?fields=name,flags
-// https://pokeapi.co/api/v2/ability/7/
-// https://api.nasa.gov/planetary/
-// https://jsonplaceholder.typicode.com/posts/1
+const myAPI = {
+  joke: "https://api.chucknorris.io/jokes/random",
+  grocery: "https://simple-grocery-store-api.glitch.me",
+  grocery: "https://simple-grocery-store-api.glitch.me",
+  bored: "https://www.boredapi.com/api/",
+  alpha: "https://jsonplaceholder.typicode.com/posts",
+  poke: "https://pokeapi.co/api/v2/ability/7/",
+  user: "https://randomuser.me/api/",
+  flag: "https://restcountries.com/v3.1/all?fields=name,flags",
+};
 
-const norrisAPI = async () => {
-  const response = await fetch("https://api.chucknorris.io/jokes/random");
+const norrisAPI = async (endpoint) => {
+  const response = await fetch(endpoint);
   const random = await response.json();
   console.log("Joke:", random.value);
 };
 
-norrisAPI()
+// norrisAPI(myAPI.joke);
 
-const myAPI = async () => {
-  const response = await fetch("https://simple-grocery-store-api.glitch.me");
+const groceryAPI = async (endpoint) => {
+  const response = await fetch(endpoint);
   const result = await response.json();
   console.log(result);
 };
-// myAPI();
+// groceryAPI(myAPI.grocery);
 
-const callAPI = () => {
-  fetch("https://www.boredapi.com/api/")
+const boredAPI = (endpoint) => {
+  fetch(endpoint)
     .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((error) => console.log("error message:", error));
 };
 
-// callAPI()
+// boredAPI(myAPI.bored);
 
-const alphaAPI = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+const alphaAPI = async (endpoint) => {
+  const response = await fetch(endpoint);
   const result = await response.json();
   console.log(result);
 };
-// alphaAPI();
+// alphaAPI(myAPI.alpha);
 
-const newAPI = () => {
-  fetch("https://pokeapi.co/api/v2/ability/7/")
+const pokeAPI = (endpoint) => {
+  fetch(endpoint)
     .then((response) => response.json())
     .then((result) => console.log(result))
     .catch((error) => console.log("error:", error));
 };
 
-// newAPI()
+// pokeAPI(myAPI.poke)
+
+const randomAPI = async (key) => {
+  const response = await fetch(key, {
+    method: "GET",
+  });
+  const result = await response.json();
+  console.log(result);
+};
+// randomAPI(myAPI.user)
+
+// Method:GET
+
+const flagAPI = async (endpoint) => {
+  const response = await fetch(endpoint, {
+    method: "GET",
+  });
+  const result = await response.json();
+  console.log(result);
+};
+// flagAPI(myAPI.flag +"/name")
+
+// ? Method:POST
+
+const postRequest = async (endpoint) => {
+  const requests = await fetch(endpoint, {
+    method: "POST",
+    body: JSON.stringify({
+      id: 101,
+      country: "Italy",
+      division: "Liga:A",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+  const result = await requests.json();
+  console.log(result);
+};
+postRequest(myAPI.alpha);
